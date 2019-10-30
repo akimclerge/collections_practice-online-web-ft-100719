dd0e66df07bb8e5134eae46970805f72da98464e
Akim Clerge <akimclerge@gmail.com>
2019-10-30 17:16:10 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(array)
   array.sort
 end
 
-sort_array_asc([25, 7, 1]) #[1,7,25]
+sort_array_asc([25, 7, 1]) 
 
 def sort_array_desc(array)
   array.sort do | left, right|
@@ -10,7 +10,7 @@ def sort_array_desc(array)
   end
 end
 
-sort_array_desc([25, 7, 14]) #[25, 14, 7]
+sort_array_desc([25, 7, 14])
 
 def sort_array_char_count(array)
   array.sort do |left, right|
@@ -18,29 +18,25 @@ def sort_array_char_count(array)
   end
 end
 
-sort_array_char_count(["dogs", "cat", "Horses"]) #["cat", "dogs", "Horses"]
-
+sort_array_char_count(["dogs", "cat", "Horses"]) 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-swap_elements(["blake", "ashley", "scott"]) #["blake", "scott", "ashley"]
-
+swap_elements(["blake", "ashley", "scott"])
 def reverse_array(array)
   array.reverse
 end
 
-reverse_array(["blake", "ashley", "scott"]) #["scott", "ashley", "blake"]
-
+reverse_array(["blake", "ashley", "scott"]) 
 def kesha_maker(array)
   array.each do |item|
     item[2] = "$"
   end
 end
 
-kesha_maker(["blake", "ashley", "scott"]) #["bl$ke", "as$ley", "sc$tt"]
-
+kesha_maker(["blake", "ashley", "scott"]) 
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
@@ -52,7 +48,7 @@ def find_a(array)
     # end
 end
 
-find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) #["apple", "avis", "arlo", "ascot"]
+find_a(["apple", "orange", "pear", "avis", "arlo", "ascot" ]) 
 
 def sum_array(array)
   sum = 0
@@ -63,7 +59,7 @@ def sum_array(array)
 end 
   
 
-sum_array([11,4,7,8,9,100,134]) #273
+sum_array([11,4,7,8,9,100,134])
 
 def add_s(array)
   array.collect do |word|
@@ -75,7 +71,7 @@ def add_s(array)
   end
 end
 
-add_s(["hand","feet", "knee", "table"]) #["hands","feet", "knees", "tables"]
+add_s(["hand","feet", "knee", "table"]) 
 def sort_array_asc(array)
   array.sort
 end
